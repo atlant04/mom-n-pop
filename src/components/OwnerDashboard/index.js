1cfae1e0d325a1f903055280a64c4573c3606d0e
Jinseo Park <jinseo@google.com>
2020-10-17 16:09:00 +0200

    React-ify blueprint stuff

--- src/components/OwnerDashboard/index.js
+++ src/components/OwnerDashboard/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './stylesheet.scss';
+import Blueprint3D from '../Blueprint3D';
 
 function OwnerDashboard() {
   return (
     <div className="OwnerDashboard">
       Owner Dashboard
+      <Blueprint3D floor={false} />
+      <button>
+        Toggle
+      </button>
     </div>
   );
 }
